8d7fc17508e49a803a064e04c610618ad1900946
Ed <edwoulfe@gmail.com>
2020-09-17 09:28:15 -0500

    Finished Signin route. Error checking for user not found, incorrect password, and server errors. Checking for the email and password, run a validPassword function from the user schema which checks the hashed passwords. On valid sign in we assign a user session token id.

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
-const {
-    request,
-    response
-} = require('express');
 let User = require('../../models/user.model');
-
+let UserSession = require('../../models/userSession');
 
 //  GET Routes  \\
 
@@ -23,9 +19,6 @@ router.route('/api/account/:id').get((request, response) => {
         .catch(err => response.status(400).json('Error: ' + err));
 })
 
-router.route('api/account/signin').get((request, response, next) => {
-
-})
 
 
 // POST Routes  \\
@@ -44,12 +37,12 @@ router.route('/api/account/signup').post((request, response, next) => {
 
     }, (err, previousUsers) => {
         if (err) {
-            return response.send({
+            return response.status(500).send({
                 success: false,
                 message: 'Error: Server error'
             });
         } else if (previousUsers.length > 0) {
-            return response.send({
+            return response.status(400).send({
                 success: false,
                 message: 'Error: Account already exists.'
             });
@@ -70,6 +63,65 @@ router.route('/api/account/signup').post((request, response, next) => {
             .catch(err => response.status(400).json('Error: ' + err));
 
     });
+
+
+
+})
+
+router.route('/api/account/signin').post((request, response, next) => {
+    let email = request.body.email;
+    let password = request.body.password;
+
+
+
+    User.findOne({
+        email: email
+
+    }, (err, user) => {
+        if (err) {
+            return response.status(500).send({
+                success: false,
+                message: "Server Error."
+            })
+        }
+        if (!user) {
+            return response.status(404).send({
+                success: false,
+                message: "User was not found."
+            })
+        }
+
+
+
+
+        if (!user.validPassword(password)) {
+            return response.status(404).send({
+                success: false,
+                message: "Password was invalid."
+            })
+        }
+
+        let userSession = new UserSession();
+        userSession.userid = user.id;
+        userSession.save((err, doc) => {
+            if (err) {
+                return response.status(500).send({
+                    success: false,
+                    message: "Server Error."
+                })
+            }
+            return response.status(200).send({
+                success: true,
+                message: "Valid sign in",
+                token: doc._id
+            })
+
+        })
+
+    })
+
+
+
 
 
 
