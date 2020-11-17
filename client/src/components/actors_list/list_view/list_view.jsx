1d3494e1541bb2ca6c148ca2d9062a1fa9abebaa
Ed <edwoulfe@gmail.com>
2020-11-17 09:59:19 -0500

    List view actor image, actor name, and actor movies now displaying in the list view container. Also added styling to get the look correct according to the design file. Added movies objects to the actor object so we can get which movies the actor is known for.

--- client/src/components/actors_list/list_view/list_view.jsx
+++ client/src/components/actors_list/list_view/list_view.jsx
@@ -4,18 +4,39 @@ import './list_view.scss';
 
 function ListView(props) {
 
+    function getKnownMovies(actor) {
+        if (actor.name === "Elijah Wood") {
+            return (
+                <div className="actor_known_movies">
+                    <img className="known_movie" src={actor.movies[0].poster} alt={actor.movies[0].title} />
+                    <img className="known_movie" src={actor.movies[1].poster} alt={actor.movies[1].title} />
+                </div>
+            )
+        }
+
+    }
+
+
+
     return (
         <div id="actors_list_view">
 
             {props.actors.map((actor, i) => (
-                <div key={i} className="actor_container_list ">
 
-                    <div id="actor_image_container" style={{ backgroundImage: `url(${actor.image})` }} >
+                <div key={i} className="actor_container_list " >
+
+                    <div className="actor_image_container" style={{ backgroundImage: `url(${actor.image})` }} >
                     </div>
 
 
                     <div className="actor_information_list">
-                        <div className="actor_name_container">
+
+                        {getKnownMovies(actor)}
+
+
+
+
+                        <div className="actor_name_container_list">
                             <h2 className="actor_name">{actor.name}</h2>
                         </div>
                         <div className="details_container_list">
@@ -25,9 +46,10 @@ function ListView(props) {
                 </div>
 
 
-            ))}
+            ))
+            }
 
-        </div>
+        </div >
 
     )
 }
