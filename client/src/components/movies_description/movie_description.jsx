70aa577761c5bb26875ceb9098f54804f80fd9d2
Ed <edwoulfe@gmail.com>
2020-12-15 13:47:20 -0500

    Added background image, movie title, movie director, ratings, and release date from the API to display on the description page. Currently using opacity for the background image, but will probably look better has an overlay.

--- client/src/components/movies_description/movie_description.jsx
+++ client/src/components/movies_description/movie_description.jsx
@@ -3,10 +3,37 @@ import './movie_description.scss';
 
 
 function MovieDescription(props) {
+    function formatDate(date) {
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let dt = date.getDate();
+        console.log(dt)
+
+        if (dt < 10) {
+            dt = '0' + dt;
+        }
+        if (month < 10) {
+            month = '0' + month;
+        }
+
+        return (month + '/' + dt + '/' + year);
+
+
+    }
     return (
-        <div id="movie_description_container">
-            <h1>{props.location.state.movie.title}</h1>
-        </div>
+
+        <div div id="movie_description_container" >
+            <div id="movie_banner_overlay">
+                <div id="movie_banner" style={{ backgroundImage: `url(${props.location.state.movie.poster})` }}>
+                </div>
+                <div id="movie_banner_information">
+                    <h1 id="movie_title">{props.location.state.movie.title}</h1>
+                    <h5 id="movie_director">{props.location.state.movie.director}</h5>
+                    <h5 id="movie_release_date">{formatDate(new Date(props.location.state.movie.year))}</h5>
+                    <h5 id="movie_rating">{(props.location.state.movie.rating)}</h5>
+                </div>
+            </div>
+        </div >
     )
 
 }
