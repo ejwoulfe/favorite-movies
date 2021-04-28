ed62123c1701c185b8d56829e65acc112cd9cb3e
Ed <edwoulfe@gmail.com>
2021-04-28 11:16:32 -0400

    Added auth required to creating and deleteing movies, will use those routes for when a user wants to add or delete from their favorite movies list.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,28 +1,23 @@
-import jwt from 'jsonwebtoken';
-import config from '../config';
+import jwt from "jsonwebtoken";
 
-const {
-    JWT_SECRET
-} = config;
+export default function auth(req, res, next) {
+  const token = req.header("x-auth-token");
 
-export default (req, res, next) => {
-    const token = req.header('x-auth-token');
+  // Check for token
+  if (!token)
+    return res.status(401).json({
+      msg: "No token, authorization denied",
+    });
 
-    // Check for token
-    if (!token)
-        return res.status(401).json({
-            msg: 'No token, authorization denied'
-        });
-
-    try {
-        // Verify token
-        const decoded = jwt.verify(token, JWT_SECRET);
-        // Add user from payload
-        req.user = decoded;
-        next();
-    } catch (e) {
-        res.status(400).json({
-            msg: 'Token is not valid'
-        });
-    }
-};
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // Add user from payload
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({
+      msg: "Token is not valid",
+    });
+  }
+}
--- backend/routes/api/movies.js
+++ backend/routes/api/movies.js
@@ -1,28 +1,27 @@
-const router = require('express').Router();
-let Movie = require('../../models/movie.model');
+  const router = require("express").Router();
+  let Movie = require("../../models/movie.model");
+  const auth = require('../../middleware/auth');
 
+  //  GET Routes  \\
 
-// GET Routes  \\
-
-router.route('/api/').get((request, response) => {
+  router.route("/api/").get((request, response) => {
     Movie.find()
-        .populate('actors')
-        .then(movies => response.json(movies))
-        .catch(err => response.status(400).json('Error: ' + err));
-});
+      .populate("actors")
+      .then((movies) => response.json(movies))
+      .catch((err) => response.status(400).json("Error: " + err));
+  });
 
-
-router.route('/api/:id').get((request, response) => {
+  router.route("/api/:id").get((request, response) => {
     Movie.findById(request.params.id)
-        .populate('actors')
-        .then(movies => response.json(movies))
-        .catch(err => response.status(400).json('Error: ' + err));
-})
+      .populate("actors")
+      .then((movies) => response.json(movies))
+      .catch((err) => response.status(400).json("Error: " + err));
+  });
 
+  //  CREATE Routes  \\
 
-// CREATE Routes  \\
+  router.route("/api/new", auth).post((request, response) => {
 
-router.route('/api/new').post((request, response) => {
     const title = request.body.title;
     const year = Date.parse(request.body.year);
     const director = request.body.director;
@@ -31,51 +30,50 @@ router.route('/api/new').post((request, response) => {
     const poster = request.body.poster;
     const actors = [];
     const newMovie = new Movie({
-        title,
-        year,
-        director,
-        rating,
-        description,
-        poster,
-        actors
+      title,
+      year,
+      director,
+      rating,
+      description,
+      poster,
+      actors,
     });
 
-    newMovie.save()
-        .then(() => response.json('Movie added.'))
-        .catch(err => response.status(400).json('Error: ' + err));
+    newMovie
+      .save()
+      .then(() => response.json("Movie added."))
+      .catch((err) => response.status(400).json("Error: " + err));
+  });
 
+  //  UPDATE Route  \\
 
-})
+  router.route("/api/update/:id").post((request, response) => {
 
-// UPDATE Route  \\
-
-router.route('/api/update/:id').post((request, response) => {
     Movie.findById(request.params.id)
-        .then(movie => {
-            movie.title = request.body.title;
-            movie.year = Date.parse(request.body.year);
-            movie.director = request.body.director;
-            movie.rating = request.body.rating;
-            movie.description = request.body.description;
-            movie.poster = request.body.poster;
-            movie.actors = request.body.actors;
+      .then((movie) => {
+        movie.title = request.body.title;
+        movie.year = Date.parse(request.body.year);
+        movie.director = request.body.director;
+        movie.rating = request.body.rating;
+        movie.description = request.body.description;
+        movie.poster = request.body.poster;
+        movie.actors = request.body.actors;
 
-            movie.save()
-                .then(() => response.json('Movie updated.'))
-                .catch(err => response.status(400).json('Error: ' + err));
-        })
-        .catch(err => response.status(400).json('Error: ' + err));
-})
+        movie
+          .save()
+          .then(() => response.json("Movie updated."))
+          .catch((err) => response.status(400).json("Error: " + err));
+      })
+      .catch((err) => response.status(400).json("Error: " + err));
+  });
 
+  //  DELETE Routes  \\
 
-// DELETE Routes  \\
+  router.route("/api/:id", auth).delete((request, response) => {
 
-router.route('/api/:id').delete((request, response) => {
     Movie.findByIdAndDelete(request.params.id)
-        .then(() => response.json('Movie Deleted.'))
-        .catch(err => response.status(400).json('Error: ' + err));
-})
+      .then(() => response.json("Movie Deleted."))
+      .catch((err) => response.status(400).json("Error: " + err));
+  });
 
-
-
-module.exports = router;
+  module.exports = router;
