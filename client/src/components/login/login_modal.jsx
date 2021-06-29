83e33b0814df3e769a2c526d90098eba62c8d334
Ed <edwoulfe@gmail.com>
2021-06-29 12:52:36 -0400

    Login form is successfully sending email and password to the login auth route. Got successful login response.

--- client/src/components/login/login_modal.jsx
+++ client/src/components/login/login_modal.jsx
@@ -1,7 +1,54 @@
+import e from 'cors';
 import React, { useEffect, useState } from 'react';
 import './login_modal.scss';
 
 function LoginModal(props) {
+
+
+    // Retrieve all input field values, ignoring the send button, and put them into an array for validation.
+    function getInputFieldValues(form) {
+
+        let inputValuesArray = [];
+
+        for (let i = 0; i < form.length; i++) {
+
+            if (form[i].id !== "login_button") {
+
+                inputValuesArray.push(form[i].value);
+
+            }
+        }
+
+        return inputValuesArray;
+    }
+
+
+    async function authFields(event) {
+
+        event.preventDefault();
+        let formFieldValues = await getInputFieldValues(event.target);
+        let formObject = {
+            email: formFieldValues[0],
+            password: formFieldValues[1]
+        };
+
+        console.log(formObject);
+
+
+
+        await fetch('http://localhost:5000/auth/api/account/login', {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(formObject)
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result)
+            })
+
+    }
 
 
 
@@ -9,15 +56,12 @@ function LoginModal(props) {
     return (
         <div id="login_modal">
             <h1>Login</h1>
-
-            <form className="contact_form" >
-                <label htmlFor="name_input" className="form_label">Name</label>
-                <input id="name_input" className="form_field" type="text" name="name" />
+            <form className="login_form" onSubmit={authFields}>
                 <label htmlFor="email_input" className="form_label">Email</label>
                 <input id="email_input" className="form_field" type="email" name="email" />
                 <label htmlFor="form_text_area" className="form_label">Password</label>
                 <input id="password_input" className="form_field" type="password" name="password" />
-                <input id="send_button" type="submit" value="Login" />
+                <input id="login_button" type="submit" value="Login" />
             </form>
 
         </div>
